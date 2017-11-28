6945fe85888c65c88328040aa923a1e675457de3
Alec Lomas <alec@lowmess.com>
2017-11-28 13:48:58 -0700

    fix: collection name is direct parent
    forgot to separate the collection name
    
    #2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const auto_collect = opts => {
     Object.keys(files).forEach(file => {
       if (mm(file, opts.pattern).length) {
         // get name of parent directory
-        let parent = path.dirname(file)
+        let parent = path.dirname(file).split(path.sep).pop()
 
         // set parent to source file in config if file is in root
         parent = parent === '.' ? metalsmith._source : parent
